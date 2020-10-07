a39f7300e9049997a9ba20dda1f65d281a6d6cbb
rct3232 <rct3232@gmail.com>
2020-10-07 16:03:56 +0900

    Add: xml Event data

--- Assets/Resources/GameSystem/Initiating/Script/EventGenerator.cs
+++ Assets/Resources/GameSystem/Initiating/Script/EventGenerator.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class EventGenerator : MonoBehaviour
 {
+    public class EventVariableInfo
+    {
+        EventVariableInfo(){}
+        public string Name;
+        public int Direction;
+    }
+    public class EventInfo
+    {
+        EventInfo(EventVariableInfo targetVar, int advantage, int changeMin, int changeMax, int durability)
+        {
+            this.TargetVar = targetVar;
+            this.Advantage = advantage;
+            this.ChangeValueRange = new int[2];
+            this.ChangeValueRange[0] = changeMin;
+            this.ChangeValueRange[1] = changeMax;
+            this.Durability = durability;
+        }
+        private EventVariableInfo TargetVar;
+        private int Advantage;
+        private int[] ChangeValueRange;
+        private int Durability;
+
+        public EventVariableInfo GetTargetVar() {return this.TargetVar;}
+        public int GetAdvantage() {return this.Advantage;}
+        public int[] GetChangeValueRange() {return this.ChangeValueRange;}
+        public int GetDurability() {return this.Durability;}
+    }
     // Start is called before the first frame update
     void Start()
     {
