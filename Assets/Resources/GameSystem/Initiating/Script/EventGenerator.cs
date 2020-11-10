6ffb7e692deb77156e0373052538a6c18fab901f
rct3232 <rct3232@gmail.com>
2020-11-10 22:34:39 +0900

    Update

--- Assets/Resources/GameSystem/Initiating/Script/EventGenerator.cs
+++ Assets/Resources/GameSystem/Initiating/Script/EventGenerator.cs
@@ -31,15 +31,32 @@ public class EventGenerator : MonoBehaviour
         public int[] GetChangeValueRange() {return this.ChangeValueRange;}
         public int GetDurability() {return this.Durability;}
     }
+    public GameObject BaseSystem;
+    public GameObject CompanyManager;
+    CompanyValue CompanyValueCall;
+    EconomyValue EconomyValueCall;
+    LandValue LandValueCall;
+    BankValue BankValueCall;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CompanyValueCall = CompanyManager.GetComponent<CompanyManager>().GetPlayerCompanyObject().GetComponent<CompanyValue>();
+        EconomyValueCall = CompanyValueCall.GetEconomyValue().GetComponent<EconomyValue>();
+        LandValueCall = CompanyValueCall.GetLandValue().GetComponent<LandValue>();
+        BankValueCall = BaseSystem.GetComponent<BankValue>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    List<EventInfo> CheckTrigger()
+    {
+        List<EventInfo> Result = new List<EventInfo>();
+
+        return Result;
     }
 }
