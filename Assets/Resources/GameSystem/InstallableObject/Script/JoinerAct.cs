92283284cd5d416043bb1bd318e09926be2d1fc0
Ozeco-Mmem <open3050@naver.com>
2020-11-10 17:35:56 +0900

    Joiner 프리팹, 스크립트(미완성)

--- Assets/Resources/GameSystem/InstallableObject/Script/JoinerAct.cs
+++ Assets/Resources/GameSystem/InstallableObject/Script/JoinerAct.cs
@@ -11,8 +11,8 @@ public class JoinerAct : MonoBehaviour
     public GameObject AddonObject;
     InstallableObjectAct ObjectActCall;
 
-    public GameObject[] MoveDetectedObject;
-    GameObject[] MoverDetector;     //벨트 확인 센서오브젝트들
+    public GameObject[] DetectedObject;
+    GameObject[] DetectSensor;     //벨트 확인 센서오브젝트들
 
 
     public GameObject PrevBelt;
@@ -26,7 +26,7 @@ public class JoinerAct : MonoBehaviour
     GameObject TargetGoods;
     GoodsValue GoodsValueCall;
 
-    public int CurrentJoinIndex;           //
+    public int CurrentJoinIndex;           //현재 진행중인 합칠 오브젝트 인덱스
     public bool NoNeedCheckCurrentIndex;           //?
     public int MainBeltIndex = -1;
 
@@ -43,8 +43,8 @@ public class JoinerAct : MonoBehaviour
         CanProceed = false;     //진행가능 여부
         isInitialized = false;  //진행 시작 가능 여부
 
-        MoveDetectedObject = new GameObject[InputNumber];   //들어오는 벨트에 확인된 오브젝트들
-        MoverDetector = new GameObject[InputNumber];        //들어오는 벨트 확인하는 센서들
+        DetectedObject = new GameObject[InputNumber];   //들어오는 벨트에 확인된 오브젝트들
+        DetectSensor = new GameObject[InputNumber];        //들어오는 벨트 확인하는 센서들
         NextBelt = new GameObject[InputNumber];
         PrevBelt = null;
         PrevBelt2 = new GameObject[InputNumber];            //들어오는 벨트들
@@ -55,7 +55,7 @@ public class JoinerAct : MonoBehaviour
         {
             // 0 ; Left
             // 1 : Right
-            MoverDetector[i] = transform.GetChild(2).GetChild(0).GetChild(i).gameObject;        //센서 호출
+            DetectSensor[i] = transform.GetChild(2).GetChild(0).GetChild(i).gameObject;        //센서 호출
         }
     }
 
@@ -66,36 +66,31 @@ public class JoinerAct : MonoBehaviour
         {
             GetBelt();                          //벨트 초기화
 
-            if (isInitialized)                  //시작 가능
+            if (isInitialized)                  //초기화 가능
             {
                 ObjectActCall.IsWorking = true; //작업중 여부 = true
 
                 if (CanProceed)                 //진행 가능
                 {
-                    if (NextBeltActCall.NeedStop)   //다음벨트 정지필요 상태
-                    {
-                        NextBeltActCall.ChangeNeedStop(false, gameObject);  //정지 필요상태 = false
-                    }
+                    //foreach(Prev)
 
                     if (MainBeltActCall.GoodsOnBelt != null)                //메인 이전 벨트에 물품 존재
                     {
                         if (MainBeltActCall.GoodsOnBelt == TargetGoods)     // 그 물품이 타겟 물품일때
                         {
-                            if (MainBeltActCall.isCenter(-1))               //메인 벨트가 중심(?)일때
+                            if (MainBeltActCall.isCenter(-1))               //메인 벨트가 방향 일치일때
                             {
                                 Joining();                             //합치기 실행
                             }
                         }
                     }
                 }
-                else
+
+                else                                                    //진행 불가(?)
                 {
-                    if (NextBeltActCall.GoodsOnBelt != null)
+                    if (NextBeltActCall.GoodsOnBelt != null)            //출구벨트에 물건 있음
                     {
-                        if (!CanProceed)
-                        {
-                            WhereToGo();
-                        }
+                        WhereToGo();
                     }
                 }
             }
@@ -130,17 +125,17 @@ public class JoinerAct : MonoBehaviour
     bool CheckInstallCondition()
     {
         bool result = true;
-        int PrevBeltCount = 0;
-        int PrevBeltIndex = -1;
+        int NextBeltCount = 0;
+        int NextBeltIndex = -1;     //들어오는 2개 벨트중 이후벨트가 연결되어있는 벨트 인덱스
 
         CheckDirection();
 
-        for (int i = 0; i < InputNumber; i++)  //나가는 벨트마다
+        for (int i = 0; i < InputNumber; i++)  //들어오는 벨트마다
         {
-            GameObject Mover = MoverDetector[i].GetComponent<ObjectAttachmentDetector>().DetectedObject;        //부착된 오브젝트 불러오기(벨트)
-            if (Mover != null)      //연결된 벨트 오브젝트 있음
+            GameObject DetectedBelt = DetectSensor[i].GetComponent<ObjectAttachmentDetector>().DetectedObject;        //부착된 오브젝트 불러오기(벨트)
+            if (DetectedBelt != null)      //연결된 벨트 오브젝트 있음
             {
-                BeltAct BeltActCall = Mover.GetComponent<BeltAct>();// 벨트 오브젝트 스크립트 불러오기
+                BeltAct BeltActCall = DetectedBelt.GetComponent<BeltAct>();// 벨트 오브젝트 스크립트 불러오기
 
                 if (BeltActCall.BeltDirection != JoinerDirection)   //1. 방향 다르면 실패
                 {
@@ -148,38 +143,37 @@ public class JoinerAct : MonoBehaviour
                     break;
                 }
 
-                if (BeltActCall.PrevBelt != null)                   //2. 이전 벨트 존재 => 이전의 벨트는 총 1개여야 함
+                if (BeltActCall.NextBelt != null)                   //2. 연결된 벨트의 이후 벨트 존재 => Joiner 이전에 연결되어있던 벨트 존재
                 {
-                    if (PrevBeltCount > 0)                          //2-1.이전 밸트개수 2개 이상시 실패
+                    if (NextBeltCount > 0)                          //2-1.이후 벨트가 이미 세팅되어있으면 => 이후 벨트가 2개 이상시 실패
                     {
                         result = false;                             
-                        PrevBeltIndex = -1;
+                        NextBeltIndex = -1;
                         break;
                     }
 
-                                                                    //연결된 벨트 이전 벨트 없음
-                    BeltAct PrevBeltAct = BeltActCall.PrevBelt.GetComponent<BeltAct>(); //이전 벨트 스크립트
+                                                                    //연결된 벨트 이후 벨트 1개
+                    BeltAct NextBeltAct = BeltActCall.NextBelt.GetComponent<BeltAct>(); //이후 벨트 스크립트
 
-                    if (PrevBeltAct.BeltDirection != JoinerDirection)       //이전 벨트 방향 확인
+                    if (NextBeltAct.BeltDirection != JoinerDirection)       //이후 벨트 방향 확인
                     {
                         result = false;
                         break;
                     }
 
-                    if (PrevBeltAct.PrevBelt != null)                       //이전 벨트의 이전 벨트 존재
+                    if (NextBeltAct.NextBelt != null)                       //이후 벨트의 이후 벨트 존재
                     {
-                        if (PrevBeltAct.PrevBelt.GetComponent<BeltAct>().BeltDirection != JoinerDirection)  //방향 확인
+                        if (NextBeltAct.PrevBelt.GetComponent<BeltAct>().BeltDirection != JoinerDirection)  //방향 확인
                         {
                             result = false;
                             break;
                         }
                     }
 
-                    PrevBeltIndex = i;
-                    PrevBeltCount++;
+                    NextBeltIndex = i;
+                    NextBeltCount++;
                 }
-            }
-                
+            }                
             else        //연결된 벨트 없음
             {
                 result = false;
@@ -187,10 +181,10 @@ public class JoinerAct : MonoBehaviour
             }
         }
 
-        if (PrevBeltIndex != -1)
+        if (NextBeltIndex != -1)        //이후벨트 연결된게 있음
         {
             Transform StructCarrier = ObjectActCall.StructObject.transform.parent;
-            string StructName = "Joiner" + InputNumber.ToString() + "-" + PrevBeltIndex.ToString();
+            string StructName = "Joiner" + InputNumber.ToString() + "-" + NextBeltIndex.ToString();
             if (ObjectActCall.StructObject.name != StructName)
             {
                 GameObject newStruct = GameObject.Instantiate(Resources.Load<GameObject>("GameSystem/InstallableObject/Struct/" + StructName), StructCarrier);
@@ -199,7 +193,7 @@ public class JoinerAct : MonoBehaviour
                 ObjectActCall.StructObject = newStruct;
             }
         }
-        else
+        else                            //
         {
             result = false;
         }
@@ -213,8 +207,8 @@ public class JoinerAct : MonoBehaviour
 
         for (int i = 0; i < InputNumber; i++)   //들어오는 벨트별로 반복
         {
-            MoveDetectedObject[i] = MoverDetector[i].GetComponent<ObjectAttachmentDetector>().DetectedObject;       //센서에 감지된 오브젝트 인식
-            BeltAct TargetBeltAct = MoveDetectedObject[i].GetComponent<BeltAct>();  //감지된 오브젝트 target으로 호출
+            DetectedObject[i] = DetectSensor[i].GetComponent<ObjectAttachmentDetector>().DetectedObject;       //센서에 감지된 오브젝트 인식
+            BeltAct TargetBeltAct = DetectedObject[i].GetComponent<BeltAct>();  //감지된 오브젝트 target으로 호출
 
             TargetBeltAct.ModuleObject = gameObject;    //벨트의 module에 joiner 입력
 
@@ -234,7 +228,7 @@ public class JoinerAct : MonoBehaviour
 
         for (int i = 0; i < InputNumber; i++)           //들어오는 벨트별로 반복
         {
-            MoveDetectedObject[i].GetComponent<BeltAct>().NextBelt = NextBelt2;      //이후벨트 하나로 입력
+            DetectedObject[i].GetComponent<BeltAct>().NextBelt = NextBelt2;      //이후벨트 하나로 입력
         }
 
         isInitialized = CanInitialize;
@@ -244,16 +238,16 @@ public class JoinerAct : MonoBehaviour
     {
         if (AddonObject == null)
         {
-            for (int i = 0; i < MoveDetectedObject.Length; i++)     //출력 벨트 내에서 반복
+            for (int i = 0; i < DetectedObject.Length; i++)     //출력 벨트 내에서 반복
             {
                 int Nextindex = CurrentJoinIndex + i;       //개수 내에서 반복
-                if (Nextindex >= MoveDetectedObject.Length)          //
+                if (Nextindex >= DetectedObject.Length)          //
                 {
-                    Nextindex -= MoveDetectedObject.Length;
+                    Nextindex -= DetectedObject.Length;
                 }
 
 
-                BeltAct indexBeltActCall = MoveDetectedObject[Nextindex].GetComponent<BeltAct>();        //출력벨트
+                BeltAct indexBeltActCall = DetectedObject[Nextindex].GetComponent<BeltAct>();        //출력벨트
 
                 if (Nextindex == MainBeltIndex)                                             //바뀔 벨트가 메인벨트
                 {
@@ -313,7 +307,7 @@ public class JoinerAct : MonoBehaviour
         }
         else        //애드온 존재
         {
-            if (MoveDetectedObject[CurrentJoinIndex].GetComponent<BeltAct>().GoodsOnBelt == null)   //현재 벨트에 물품 없으면 진행 가능
+            if (DetectedObject[CurrentJoinIndex].GetComponent<BeltAct>().GoodsOnBelt == null)   //현재 벨트에 물품 없으면 진행 가능
             {
                 CanProceed = true;  
                 return;
@@ -361,14 +355,15 @@ public class JoinerAct : MonoBehaviour
         if (MainBeltIndex != CurrentJoinIndex)     //현재 진행되는 벨트가 메인벨트가 아님
         {
             MainBeltActCall.GoodsOnBelt.transform.position =
-                new Vector3(NextBelt2.transform.position.x, MoveDetectedObject[CurrentJoinIndex].transform.position.y + 0.325f, NextBelt2.transform.position.z);      //메인벨트의 물품 이동
-            MoveDetectedObject[CurrentJoinIndex].GetComponent<BeltAct>().GoodsOnBelt = MainBeltActCall.GoodsOnBelt;     //이후의 벨트의 물품 = 
+                new Vector3(NextBelt2.transform.position.x, DetectedObject[CurrentJoinIndex].transform.position.y + 0.325f, NextBelt2.transform.position.z);      //메인벨트의 물품 이동
+            DetectedObject[CurrentJoinIndex].GetComponent<BeltAct>().GoodsOnBelt = MainBeltActCall.GoodsOnBelt;     //이전의 벨트의 물품 = 
             MainBeltActCall.GoodsOnBelt = null;
 
             TargetGoods = null;
             NoNeedCheckCurrentIndex = false;
             CanProceed = false;
         }
+
         else
         {
             TargetGoods = null;
@@ -379,7 +374,7 @@ public class JoinerAct : MonoBehaviour
         if (AddonObject == null)
         {
             CurrentJoinIndex += 1;
-            if (CurrentJoinIndex >= MoveDetectedObject.Length)
+            if (CurrentJoinIndex >= DetectedObject.Length)
             {
                 CurrentJoinIndex = 0;
             }
@@ -395,10 +390,10 @@ public class JoinerAct : MonoBehaviour
 
         for (int i = 0; i < InputNumber; i++)
         {
-            if (i != MainBeltIndex) MoveDetectedObject[i].GetComponent<BeltAct>().PrevBelt = null;
+            if (i != MainBeltIndex) DetectedObject[i].GetComponent<BeltAct>().PrevBelt = null;
 
-            MoveDetectedObject[i].GetComponent<BeltAct>().ChangeNeedStop(false, gameObject);
-            MoveDetectedObject[i].GetComponent<BeltAct>().ModuleObject = null;
+            DetectedObject[i].GetComponent<BeltAct>().ChangeNeedStop(false, gameObject);
+            DetectedObject[i].GetComponent<BeltAct>().ModuleObject = null;
         }
 
         if (PrevBelt != null)
